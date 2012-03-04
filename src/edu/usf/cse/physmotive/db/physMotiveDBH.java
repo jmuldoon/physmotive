62c755bdf7f116e8ac050f42d296e727e3c5c581
Ecoles88 <Ecoles88@gmail.com>
2012-03-04 01:49:18 -0500

    Started building the diaryList page. Setup database information started converting files to new names.

--- src/edu/usf/cse/physmotive/db/physMotiveDBH.java
+++ src/edu/usf/cse/physmotive/db/physMotiveDBH.java
@@ -1,6 +1,49 @@
 package edu.usf.cse.physmotive.db;
 
-public class physMotiveDBH
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class physMotiveDBH extends SQLiteOpenHelper
 {
+
+    static final String DATABASE_NAME = "physmotive";
+    static final int DATABASE_VERSION = 1;
+
+    static final String CREATE_DIARY = "create table diary (_id integer primary key autoincrement,"
+	    + " name text not null, height float, weight float, age integer, gender integer,"
+	    + " note text, entryUsr integer not null, entryDate integer not null,"
+	    + " updateUsr integer, updateDate integer, deleted integer);";
+
+    // Only created so I can make a list of activities, Please update as
+    // necessary. I will be using EntryDate as the name of the activity.
+    static final String CREATE_ACTIVITY = "create table activity (_id integer primary key autoincrement,"
+	    + " name text not null, entryUsr integer not null, entryDate integer not null,"
+	    + " updateUsr integer, updateDate integer, deleted integer);";
+
+    static final String CREATE_USER = "create table users (_id integer primary key autoincrement,"
+	    + " firstName text not null, lastName text not null, height float, weight float,"
+	    + " age integer, gender integer, units integer, orientation integer, dareFormat integer,"
+	    + " entryUsr integer not null, entryDate integer not null, updateUsr integer,"
+	    + " updateDate integer, deleted integer);";
+
+    physMotiveDBH(Context context) {
+	super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+	db.execSQL(CREATE_DIARY);
+	db.execSQL(CREATE_ACTIVITY);
+	db.execSQL(CREATE_USER);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVerison)
+    {
+	db.execSQL("Drop Table If Exists MT_Collections");
+	onCreate(db);
+    }
 
 }
