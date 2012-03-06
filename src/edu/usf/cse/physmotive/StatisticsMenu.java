f6a7cb2707bbc56e3481f20468b5ad2a122078b0
James Muldoon <jmuldoon@mail.usf.edu>
2012-03-06 00:56:58 -0500

    got the dialogue box for new user to work, fixed a few bugs.

--- src/edu/usf/cse/physmotive/StatisticsMenu.java
+++ src/edu/usf/cse/physmotive/StatisticsMenu.java
@@ -1,7 +1,14 @@
 package edu.usf.cse.physmotive;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class StatisticsMenu extends Activity{
-	//TO DO: jmuldoon
+	// Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistic_menu);
+        
+     }
 }
