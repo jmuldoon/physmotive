54a6c6759f116cba4946ddb00cfcbbbc9f73739f
Nick <maxwell2@mail.usf.edu>
2012-03-05 20:35:02 -0500

    oncreate and isroutedisplayed added

--- src/edu/usf/cse/physmotive/ActiveActivity.java
+++ src/edu/usf/cse/physmotive/ActiveActivity.java
@@ -1,7 +1,18 @@
 package edu.usf.cse.physmotive;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class ActiveActivity extends Activity {
-	// To Do: Nick
+public class ActiveActivity extends MapActivity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 }
