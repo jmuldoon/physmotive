406ba08855d22ba9e0c7adaf3d4561ab49ecab1b
Eddie Coles <ecoles1@mail.usf.edu>
2012-03-20 16:41:04 -0400

    All these changes are related to the user selection

--- src/edu/usf/cse/physmotive/db/PhysMotiveDBH.java
+++ src/edu/usf/cse/physmotive/db/PhysMotiveDBH.java
@@ -1,5 +1,9 @@
 package edu.usf.cse.physmotive.db;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,8 +22,7 @@ public class PhysMotiveDBH extends SQLiteOpenHelper
     // Only created so I can make a list of activities, Please update as
     // necessary. I will be using EntryDate as the name of the activity.
     static final String CREATE_ACTIVITY = "create table activity (_id integer primary key autoincrement,"
-    	+ " diaryId integer, checked integer,"
-    	+ " entryUsr integer not null, entryDate integer not null,"
+	    + " diaryId integer, checked integer," + " entryUsr integer not null, entryDate integer not null,"
 	    + " updateUsr integer, updateDate integer, deleted integer);";
 
     static final String CREATE_USER = "create table users (_id integer primary key autoincrement,"
@@ -27,6 +30,8 @@ public class PhysMotiveDBH extends SQLiteOpenHelper
 	    + " age integer, gender integer, units integer, orientation integer, dateFormat integer,"
 	    + " entryUsr integer not null, entryDate integer not null, updateUsr integer,"
 	    + " updateDate integer, deleted integer);";
+
+    static final String DEFAULT_USER = "Insert into users ()";
 
     PhysMotiveDBH(Context context) {
 	super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,6 +43,8 @@ public class PhysMotiveDBH extends SQLiteOpenHelper
 	db.execSQL(CREATE_DIARY);
 	db.execSQL(CREATE_ACTIVITY);
 	db.execSQL(CREATE_USER);
+	createDefaults(db);
+
     }
 
     @Override
@@ -47,4 +54,25 @@ public class PhysMotiveDBH extends SQLiteOpenHelper
 	onCreate(db);
     }
 
+    private void createDefaults(SQLiteDatabase db)
+    {
+	String timeStamp = new Timestamp(Calendar.getInstance().getTimeInMillis()).toString();
+	ContentValues values = new ContentValues();
+	values.put("firstName", "Default");
+	values.put("lastName", "User");
+	values.put("height", 0);
+	values.put("weight", 0);
+	values.put("age", 0);
+
+	values.put("gender", 0);
+	values.put("units", 0);
+	values.put("orientation", 0);
+	values.put("dateFormat", 0);
+	values.put("entryUsr", 1);
+	values.put("entryDate", timeStamp);
+	values.put("updateUsr", 0);
+	values.put("updateDate", timeStamp);
+	values.put("deleted", 0);
+	db.insert("users", null, values);
+    }
 }
