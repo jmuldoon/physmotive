e6fbf17c420abd0b56fda225cd41518ef59860b6
James Muldoon <jmuldoon@mail.usf.edu>
2012-03-21 14:15:15 -0400

    created the DBM for the location based services. Eddy you might want to have a once over on it to see if i missed anything, but from what I can tell it is pretty straight forward. I don't think we will ever need to do updating but deleting and inserting is a must of course.

---
+++ src/edu/usf/cse/physmotive/db/LocationDBM.java
@@ -0,0 +1,112 @@
+package edu.usf.cse.physmotive.db;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class LocationDBM {
+	static final String TABLENAME = "locations";
+	static final String ID = "_id";
+	static final String FKEY = "race_id";
+	static final String LATITUDE = "lat";
+	static final String LONGITUDE = "lng";
+	static final String LTS = "locationTimeStamp";
+	static final String EUSR = "entryUsr";
+    static final String EDATE = "entryDate";
+    static final String UUSR = "updateUsr";
+    static final String UDATE = "updateDate";
+	static final String DEL = "deleted";
+	
+	private final Context androidContext;
+	private PhysMotiveDBH dbHelper;
+	private SQLiteDatabase db;
+
+	public LocationDBM(Context ctx) {
+		this.androidContext = ctx;
+	}
+
+	public void open() throws SQLException {
+		dbHelper = new PhysMotiveDBH(androidContext);
+		db = dbHelper.getWritableDatabase();
+	}
+
+	public void close() {
+		db.close();
+		dbHelper.close();
+	}
+
+	public SQLiteDatabase getDB() {
+		return db;
+	}
+
+	public int insert(String raceID, String lat, String lng, long usr) {
+		String timeStamp = new Timestamp(Calendar.getInstance().getTimeInMillis()).toString();
+		ContentValues values = new ContentValues();
+
+		values.put(FKEY, raceID);
+		values.put(LATITUDE, lat);
+		values.put(LONGITUDE, lng);
+		values.put(LTS, timeStamp);
+		values.put(EUSR, usr);
+		values.put(EDATE, timeStamp);
+		values.put(UUSR, usr);
+		values.put(UDATE, timeStamp);
+		values.put(DEL, 0);
+
+		return (int) db.insert(TABLENAME, null, values);
+	}
+
+	// Don't think we will ever be updating race data after it is ran. May be able to get away without this.
+	public boolean update(int id, String lat, String lng, long usr) {
+		String timeStamp = new Timestamp(Calendar.getInstance().getTimeInMillis()).toString();
+		String whereClause = ID + "=" + id;
+		ContentValues values = new ContentValues();
+
+		values.put(LATITUDE, lat);
+		values.put(LONGITUDE, lng);
+		values.put(LTS, timeStamp);
+		values.put(UUSR, usr);
+		values.put(UDATE, timeStamp);
+		
+		return db.update(TABLENAME, values, whereClause, null) > 0;
+	}
+
+	
+	public boolean delete(long id, long raceID, long usr) {
+		ContentValues values = new ContentValues();
+		String whereClause = ID + "=" + id + "and" + FKEY + "=" + raceID;
+		String timeStamp = new Timestamp(Calendar.getInstance().getTimeInMillis()).toString();
+
+		values.put(DEL, 1);
+		values.put(UUSR, usr);
+		values.put(UDATE, timeStamp);
+
+		return db.update(TABLENAME, values, whereClause, null) > 0;
+	}
+
+	public Cursor get(long id) {
+		String[] columns = new String[] { ID, EUSR, EDATE, UUSR, UDATE, DEL };
+		String whereClause = ID + "=" + id;
+		Cursor mCursor = db.query(TABLENAME, columns, whereClause, null, null,
+				null, null, null);
+		if (mCursor != null)
+			mCursor.moveToFirst();
+
+		return mCursor;
+	}
+
+	public Cursor getList(long userId) {
+		String[] columns = new String[] { ID, FKEY, LATITUDE, LONGITUDE, LTS, UDATE };
+		String whereClause = DEL + " <> 1";
+		Cursor c = db.query(TABLENAME, columns, whereClause, null, null, null,
+				null, null);
+		c.moveToFirst();
+
+		return c;
+	}
+}
