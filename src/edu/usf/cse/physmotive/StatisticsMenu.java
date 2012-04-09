36f2d0cc30ddcb51f1f55b19d5cd78423a60dc83
Nick <maxwell2@mail.usf.edu>
2012-04-09 19:36:26 -0400

    Some more graph stuff and backgrounds

--- src/edu/usf/cse/physmotive/StatisticsMenu.java
+++ src/edu/usf/cse/physmotive/StatisticsMenu.java
@@ -75,6 +75,7 @@ public class StatisticsMenu extends Activity
         totalTimeTextView = (TextView) findViewById(R.id.totalTimeTextView);
         totalDistanceTextView = (TextView) findViewById(R.id.totalDistanceTextView);
         bmiTextView = (TextView) findViewById(R.id.BMITextView);
+        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
 
         // Setting up Radio Buttons
         filterDatePicker = (DatePicker) findViewById(R.id.filterDatePicker);
@@ -93,22 +94,15 @@ public class StatisticsMenu extends Activity
         // TODO: Graph??
         // TODO: create stats, live data
         
-        // Initialize our XYPlot reference:
-        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
  
         // Create two arrays of y-values to plot:
-        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+        Number[] series1Numbers = {};
  
         // Turn the above arrays into XYSeries:
         XYSeries series1 = new SimpleXYSeries(
                 Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
                 SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                "Series1");                             // Set the display title of the series
- 
-        // Same as above, for series2
-        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, 
-                "Series2");
+                "Distance");                             // Set the display title of the series
  
         // Create a formatter to use for drawing a series using LineAndPointRenderer:
         LineAndPointFormatter series1Format = new LineAndPointFormatter(
@@ -119,9 +113,8 @@ public class StatisticsMenu extends Activity
         // Add series1 to the xyplot:
         mySimpleXYPlot.addSeries(series1, series1Format);
  
-        // Same as above, with series2:
-        mySimpleXYPlot.addSeries(series2, new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100),
-                Color.rgb(150, 150, 190)));
+        mySimpleXYPlot.setDomainLabel("Time");
+        mySimpleXYPlot.setRangeLabel("Distance");
  
  
         // Reduce the number of range labels
